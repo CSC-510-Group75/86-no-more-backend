cb87bd9fc1f27840ffc79e82fb2aea4dfdc3e841
nikhita003 <179535317+nikhita003@users.noreply.github.com>
2024-11-01 17:23:13 -0400

    Update user.js
    corrected the indentations

--- models/user.js
+++ models/user.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-username: { type: String, required: true },
-password: { type: String, required: true },
-role: { type: String, enum: ['customer', 'staff'], required: true } // New field added
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: { // New field added
+    type: String,
+    enum: ['customer', 'staff'],
+    required: true
+  }
 });
 
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('User', userSchema);
