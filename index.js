b800e3d4d83e47ecbe5a632118405d071c219550
nikhita003 <179535317+nikhita003@users.noreply.github.com>
2024-11-01 18:07:05 -0400

    integrated routes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
 const passportJWT = require('./config/passport-jwt-strategy');
-
+const dashboardRoutes = require('./routes/dashboard');
 app.use(cors({
     origin:'http://localhost:3000/'
 }));
@@ -67,7 +67,7 @@ app.use(passport.setAuthenticatedUser);
 //Use express router
 
 app.use('/',require('./routes'));
-
+app.use('/dashboard', dashboardRoutes);
 app.listen(port, function(err){
     if (err){
         console.log('Error',err)
